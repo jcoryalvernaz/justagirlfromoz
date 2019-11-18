ed353b6dea4474595e495af1a88d56abe20c0bca
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-11-18 12:59:37 -0800

    validation of image resolution

--- studio/schemas/objects/figure.js
+++ studio/schemas/objects/figure.js
@@ -1,25 +1,31 @@
 export default {
-  name: 'figure',
-  title: 'Image',
-  type: 'image',
+  name: "figure",
+  title: "Image",
+  type: "image",
   options: {
     hotspot: true
   },
+  validation: Rule =>
+    Rule.custom(image => {
+      if (!image) return true;
+      const { dimensions } = decodeAssetId(image.asset._ref);
+      return dimensions.width >= 1000 || "Image resolution must be higher";
+    }),
   fields: [
     {
-      title: 'Caption',
-      name: 'caption',
-      type: 'string',
+      title: "Caption",
+      name: "caption",
+      type: "string",
       options: {
         isHighlighted: true
       }
     },
     {
-      name: 'alt',
-      type: 'string',
-      title: 'Alternative text',
-      validation: Rule => Rule.error('You have to fill out the alternative text.').required(),
-      description: 'Important for SEO and accessiblity.',
+      name: "alt",
+      type: "string",
+      title: "Alternative text",
+      validation: Rule => Rule.error("You have to fill out the alternative text.").required(),
+      description: "Important for SEO and accessiblity.",
       options: {
         isHighlighted: true
       }
@@ -27,8 +33,21 @@ export default {
   ],
   preview: {
     select: {
-      imageUrl: 'asset.url',
-      title: 'caption'
+      imageUrl: "asset.url",
+      title: "caption"
     }
   }
-}
+};
+
+const pattern = /^image-([a-f\d]+)-(\d+x\d+)-(\w+)$/;
+
+const decodeAssetId = id => {
+  const [, assetId, dimensions, format] = pattern.exec(id);
+  const [width, height] = dimensions.split("x").map(v => parseInt(v, 10));
+
+  return {
+    assetId,
+    dimensions: { width, height },
+    format
+  };
+};
