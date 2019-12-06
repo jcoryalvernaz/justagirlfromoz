6538cbe7f9c02303c7af5eb8db7cec77e2d7a906
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-12-05 16:47:56 -0800

    removing unused imports

--- web/src/pages/archive.js
+++ web/src/pages/archive.js
@@ -7,8 +7,6 @@ import SEO from "../components/seo"
 import Layout from "../containers/layout"
 import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from "../lib/helpers"
 
-import { responsiveTitle1 } from "../components/typography.module.css"
-
 export const query = graphql`
   query ArchivePageQuery {
     projects: allSanityProject(
