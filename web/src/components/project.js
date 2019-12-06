6399458680b5ce95622ea57257641dd134ba15fd
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-12-05 16:51:34 -0800

    max height on mainImage

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -53,13 +53,15 @@ const ProjectStyles = styled.article`
   .mainImage {
     position: relative;
     background: #eee;
+    div {
+      max-height: 800px;
+    }
     img {
       position: absolute;
       top: 0;
       left: 0;
       width: 100%;
       height: 100%;
-
       object-fit: cover;
     }
   }
