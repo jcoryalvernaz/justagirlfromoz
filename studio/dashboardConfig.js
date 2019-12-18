55995f793f1deee5a77192bc049ce554c2d2d085
Jason Alvernaz <jason.alvernaz@lcb.state.nv.us>
2019-12-18 08:32:21 -0800

    branding studio

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,33 +1,27 @@
 export default {
   widgets: [
+    { name: "structure-menu" },
     {
-      name: 'sanity-tutorials',
-      options: {
-        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
-      }
-    },
-    {name: 'structure-menu'},
-    {
-      name: 'project-info',
+      name: "project-info",
       options: {
         __experimental_before: [
           {
-            name: 'netlify',
+            name: "netlify",
             options: {
               description:
-                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+                "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
               sites: [
                 {
-                  buildHookId: '5dc07afc02074c53602ac183',
-                  title: 'Sanity Studio',
-                  name: 'justagirlfromoz-studio',
-                  apiId: '50714426-f260-48f3-8f97-ea4877d6e01a'
+                  buildHookId: "5dc07afc02074c53602ac183",
+                  title: "Sanity Studio",
+                  name: "justagirlfromoz-studio",
+                  apiId: "50714426-f260-48f3-8f97-ea4877d6e01a"
                 },
                 {
-                  buildHookId: '5dc07afcdd2eba829f4cf85b',
-                  title: 'Portfolio Website',
-                  name: 'justagirlfromoz',
-                  apiId: '64f98723-51d8-456a-a5e5-e1be9ce05d96'
+                  buildHookId: "5dc07afcdd2eba829f4cf85b",
+                  title: "Portfolio Website",
+                  name: "justagirlfromoz",
+                  apiId: "64f98723-51d8-456a-a5e5-e1be9ce05d96"
                 }
               ]
             }
@@ -35,23 +29,23 @@ export default {
         ],
         data: [
           {
-            title: 'GitHub repo',
-            value: 'https://github.com/jcoryalvernaz/justagirlfromoz',
-            category: 'Code'
+            title: "GitHub repo",
+            value: "https://github.com/jcoryalvernaz/justagirlfromoz",
+            category: "Code"
           },
           {
-            title: 'Frontend',
-            value: 'https://justagirlfromoz.netlify.com',
-            category: 'apps'
+            title: "Frontend",
+            value: "https://justagirlfromoz.netlify.com",
+            category: "apps"
           }
         ]
       }
     },
-    {name: 'project-users', layout: {height: 'auto'}},
+    { name: "project-users", layout: { height: "auto" } },
     {
-      name: 'document-list',
-      options: {title: 'Recent projects', order: '_createdAt desc', types: ['project']},
-      layout: {width: 'medium'}
+      name: "document-list",
+      options: { title: "Recent projects", order: "_createdAt desc", types: ["project"] },
+      layout: { width: "medium" }
     }
   ]
-}
+};
---
+++ studio/logo/logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./logo.css";
+
+const Logo = () => (
+  <img
+    className={styles.logo}
+    src="/static/justagirlfromoz_logo.svg"
+    alt="Just a Girl from Oz Photography"
+  />
+);
+
+export default Logo;
